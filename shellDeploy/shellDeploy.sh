537e4d45e83887b31f99e22da3a1bc073f29322a
robertqiu <qiuxincsu@hotmail.com>
2019-08-29 11:13:28 +0800

    Update shellDeploy.sh

--- shellDeploy/shellDeploy.sh
+++ shellDeploy/shellDeploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+##入参MachineIP为本机的IP Address
+##Parameter IP Address is local IP Address
 MachineIp=$1
+
+##CodePath 为代码下载和编译路径
+##CodePath is path for code download and compilation
 CodePath=/usr/local/tarscode
+
+##MysqlDefaultPassword为Tars使用的默认密码, 初次安装不建议修改。修改该密码，涉及到很多配置改动。
+##MysqlDefaultPassword is the initial password for mysql opertation. Would suggest keep default password.
+##Some configurations are required to be modified once MysqlDefaultPassword is changed.   
 MysqlDefaultPassword=tars2015
 
+##检查是否输入IP地址，如果没有输入，则退出脚本
+##check whether IP address is inputed, exit if not
 if [ ! -n "$1" ] ;then
     echo "NO Local IP address Input, please input IP address."
     exit
@@ -10,12 +22,14 @@ else
     echo "Input Local IP address is $MachineIp"
 fi
 
-
+##显示脚本的运行时间
+##Display the runtime 
 time=$(date "+%Y%m%d-%H%M%S")
 echo "Install Script Run Time : ${time}"
 
-if false;then
-##install basic tools
+## 安装部署过程中需要的软件
+## install basic tools
+# if false;then
 yum install -y glibc-devel
 yum install -y flex bison
 yum install -y cmake
@@ -33,7 +47,8 @@ yum install -y git
 yum install -y expect
 yum install -y tar
 
-#install mysql
+## 安装mysql 5.7
+## install mysql 5.7
 wget https://repo.mysql.com//mysql80-community-release-el7-3.noarch.rpm
 rpm -ivh mysql80-community-release-el7-3.noarch.rpm
 yum-config-manager --disable mysql80-community
@@ -41,32 +56,32 @@ yum-config-manager --enable mysql57-community
 yum install -y mysql-community-server
 yum install -y mysql-devel
 
+## 启动mysql,并设置为开机自启动
+## start mysql, and enable mysql when system restart
+systemctl status mysqld.service
+systemctl start mysqld.service
+systemctl enable mysqld.service
 
-#check whether folder /usr/local/mysql/data exist; creat the folder if it does NOT exist
-#if [ ! -d "/usr/local/mysql/data" ]; then
-#echo "[Message]/usr/local/mysql/data does not exit"
-#mkdir -p /usr/local/mysql/data
-#echo "[Message]create a folder for /usr/local/mysql/data"
-#fi
+## 安装nvm
+## install nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.bashrc
+nvm install v8.11.3
+npm install -g pm2 --registry=https://registry.npm.taobao.org
 
-#backup mysql data,make sure no data loss in mysql
-#datename=$(date +%Y%m%d-%H%M%S)
-#mv /usr/local/mysql/data /usr/local/mysql/$datename
-#mkdir -p /usr/local/mysql/data
-
-
-
-#download the code
+## 下载Tars代码
+## download Tars code
 mkdir -p $CodePath
 cd $CodePath
 git clone https://github.com/TarsCloud/Tars.git
 cd Tars
 git submodule update --init --recursive
 
-
-#compile tars code
+## 编译Tars框架代码
+## compile Tars framework
 cd $CodePath/Tars/framework
-#modify the parameter in the CMake, make sure the compile run successfully
+## 修改Cmake中mysql配置参数，确保编译通过
+## modify the parameter in the CMake, make sure the compile run successfully
 find . -name 'CMakeLists.txt' | xargs perl -pi -e 's|/usr/local/mysql/include|/usr/include/mysql|g'
 find . -name 'CMakeLists.txt' | xargs perl -pi -e 's|/usr/local/mysql/lib|/usr/lib64/mysql|g'
 cd $CodePath/Tars/framework/tarscpp
@@ -75,8 +90,8 @@ find . -name 'CMakeLists.txt' | xargs perl -pi -e 's|/usr/local/mysql/lib|/usr/l
 cd $CodePath/Tars/framework/build
 ./build.sh all
 
-
-#install tars
+## Tars安装
+## Install Tars
 cd /usr/local/
 mkdir tars
 chown root:root ./tars/
@@ -85,57 +100,47 @@ cd $CodePath/Tars/framework/build
 
 
 
-#systemctl status mysqld.service
-systemctl start mysqld.service
-systemctl enable mysqld.service
 
-fi
-
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
-nvm install v8.11.3
-npm install -g pm2 --registry=https://registry.npm.taobao.org
+#fi
 
 
-#check whether mysqlOperation file exist
-checkfile=$CodePath/Tars/framework/sql/mysqlOperation.sh
+
+##配置Mysql参数
+##Config Mysql 
+
+##安装过成，需要cp mysqlConfig.sh到目标路径，先检查是否已经存在，如果已经存在，先删除，再cp
+##check whether mysqlConfig file exist
+checkfile=$CodePath/Tars/framework/sql/mysqlConfig.sh
 if [  -f "$checkfile" ]; then 
-rm -rf $CodePath/Tars/framework/sql/mysqlOperation.sh
-echo "get rid of the old file, copy an new file"
+rm -rf $CodePath/Tars/framework/sql/mysqlConfig.sh
+echo "delete previous file, copy an new file"
 fi 
 
-cp -rf mysqlOperation.sh  $CodePath/Tars/framework/sql 
+cp -rf mysqlConfig.sh  $CodePath/Tars/framework/sql 
 
 cd  $CodePath/Tars/framework/sql
 pwd
 
+##替换本机IP地址
+##Replace the previous configureation parameter with local IP address
 sed -i "s/192.168.2.131/$MachineIp/g" `grep 192.168.2.131 -rl ./*`
 sed -i "s/db.tars.com/$MachineIp/g" `grep db.tars.com -rl ./*`
 sed -i "s/10.120.129.226/$MachineIp/g" `grep 10.120.129.226 -rl ./*`
 
 
-# Grab temp password
+## 登入mysql, 配置新的password,参数，数据库以及表项
+## login mysql, configure new password, parameters,database as well as table  
 TempPassword=`cat /var/log/mysqld.log |grep "temporary password" |awk -F ":" '{print $NF}'`
 echo "Temp Password:" $TempPassword
-#input password to login 
-#./mysqlOperation.sh root $TempPassword
-./mysqlOperation.sh root tars2015
-
-
-#import db_tars_web.sql
-cp importTarsWebSql.sh $CodePath/Tars/web/sql
-cd $CodePath/Tars/web/sql
-sed -i 's/db.tars.com/$MachineIp/g' config/webConf.js
-sed -i 's/registry.tars.com/$MachineIp/g' config/tars.conf
-./importTarsWebSql.sh $MysqlDefaultPassword
+./mysqlOperation.sh root $TempPassword
 
 
 
-#make the framework
+
+##安装核心基础服务
+##install the service of tars framework
 cd $CodePath/Tars/framework/build
 make framework-tar
-
-#create install path
 cd /usr/local
 mkdir app
 cd /usr/local/app/
@@ -149,16 +154,36 @@ sed -i "s/192.168.2.131/$MachineIp/g" `grep 192.168.2.131 -rl ./*`
 sed -i "s/db.tars.com/$MachineIp/g" `grep db.tars.com -rl ./*`
 sed -i "s/registry.tars.com/$MachineIp/g" `grep registry.tars.com -rl ./*`
 sed -i "s/web.tars.com/$MachineIp/g" `grep web.tars.com -rl ./*`
-
-#pull all the tars process to run
+##拉起核心基础服务
 cd /usr/local/app/tars/
 chmod u+x tars_install.sh
 ./tars_install.sh
-ps -aux |grep tars
 
+##配置tarsweb数据库以及表项
+##Config Tarsweb database
+cd $CodePath/shellDeploy
+cp importTarsWebSql.sh $CodePath/Tars/web/sql
+cd $CodePath/Tars/web/sql
+sed -i 's/db.tars.com/$MachineIp/g' config/webConf.js
+sed -i 's/registry.tars.com/$MachineIp/g' config/tars.conf
+./importTarsWebSql.sh $MysqlDefaultPassword
 
+## 安装tarsweb使用软件
+## install tarsweb software
+npm install --registry=https://registry.npm.taobao.org
+npm run prd
+
+##创建log日志目录
+## create log folder
+mkdir -p /data/log/tars
+pm2 start 0
+
+##关闭防火墙
 service firewalld status
 systemctl stop firewalld
 systemctl disable firewalld
 
-pm2 start 0
+
+
+
+
