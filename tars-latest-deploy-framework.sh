11c2617d7cfd399d7a4e0e12b699a123fda45b9a
ruanshudong <ruanshudong@qq.com>
2021-04-02 15:16:40 +0800

    add tars latest deploy

---
+++ tars-latest-deploy-framework.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if (( $# < 1 ))
+then
+    echo $#
+    echo "$0 push(false/true)"
+    exit 1
+fi
+
+push=$1
+
+if [ "$push" == "true" ]; then
+	push="true"
+else
+	push="false"
+fi
+
+cd docker/framework
+./build-docker.sh master master latest ${push}
