2e92e60ea97ee71862c05398936249c1d2c15c4b
ruanshudong <ruanshudong@qq.com>
2021-10-07 22:49:39 +0800

    fix tars-deploy-framework.sh get tag version

--- tars-deploy-framework.sh
+++ tars-deploy-framework.sh
@@ -15,9 +15,14 @@ else
 	push="false"
 fi
 
-tarsTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always)
-cd framework && frameworkTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always) && cd ..
-cd web && webTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always) && cd ..
+# tarsTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always)
+# cd framework && frameworkTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always) && cd ..
+# cd web && webTag=$(git describe --tags $(git rev-list --tags --max-count=1) --abbrev=0 --always) && cd ..
+
+tarsTag=$(git reflog HEAD | grep 'checkout:' | head -1 | awk '{print $NF}')
+cd framework && frameworkTag=$(git reflog HEAD | grep 'checkout:' | head -1 | awk '{print $NF}') && cd ..
+cd web && webTag=$(git reflog HEAD | grep 'checkout:' | head -1 | awk '{print $NF}') && cd ..
+
 echo "starting tars:${tarsTag} framework:$frameworkTag web:$webTag deploy"
 
 cd docker/framework
